b5c28f871995d023b305cefe5fec51b6b89a63ba
ivandaho <ivanho_yo@hotmail.com>
2015-10-16 23:14:47 -0400

    added obstacle generator, paused keypressing work
    It instantiates obstacles which expire after a set time

---
+++ Assets/Scripts/AvoidGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvoidGenerator : MonoBehaviour {
+
+    public GameObject obj;
+    public float interval = 1f; // interval is 1 second
+    public float spd;
+    
+    float timeToNext;
+    float startTime;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time + interval; //startTime is for countdown
+        // starttime = runtime + interval
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeToNext = startTime - Time.time;
+        if (timeToNext < 0) {
+            Instantiate(obj, transform.position, Quaternion.identity);
+
+            startTime = Time.time + interval;
+            System.Random ran = new System.Random();
+            int newInterval = ran.Next(0,150);
+            interval = (float)newInterval/100f;
+        }
+	}
+}
---
+++ Assets/Scripts/AvoidThis.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvoidThis : MonoBehaviour {
+
+    public int speed = 10;
+    public float maxDist = 25;
+    float initialY;
+
+	// Use this for initialization
+	void Start () {
+        initialY = transform.position.y;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x,
+                transform.position.y - speed * Time.deltaTime*2,
+                transform.position.z);
+        /*
+        if(transform.position.y < initialY - 25) {
+            transform.position = new Vector3(transform.position.x,
+                initialY, transform.position.z);
+        }
+    */
+	}
+    
+    void LateUpdate() {
+        if (transform.position.y <initialY - maxDist) {
+            Destroy  (this.gameObject);
+        }
+    }
+}
